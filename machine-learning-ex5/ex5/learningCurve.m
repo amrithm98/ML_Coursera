25898a229faee63a91243265c2f78df466ef8fc1
amrithm98 <amrithchallenger@gmail.com>
2016-11-05 20:11:40 +0530

    Week 6 Ex 5 Complete

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,10 +53,11 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
-
-
+for i=1:m
+	theta=trainLinearReg(X(1:i,:),y(1:i,:),lambda);
+	error_train(i)=linearRegCostFunction(X(1:i,:),y(1:i,:),theta,0);%train for 0 value of lambda
+	error_val(i)=linearRegCostFunction(Xval,yval,theta,0);%cross for 0 value of lambda
+endfor;
 
 
 % -------------------------------------------------------------
--- machine-learning-ex5/ex5/polyFeatures.m
+++ machine-learning-ex5/ex5/polyFeatures.m
@@ -14,12 +14,11 @@ X_poly = zeros(numel(X), p);
 %               column of X contains the values of X to the p-th power.
 %
 % 
-
-
-
-
-
-
+temp=X;
+for i=2:p
+	temp=horzcat(temp,X.^i);
+endfor;
+X_poly=temp;
 % =========================================================================
 
 end
